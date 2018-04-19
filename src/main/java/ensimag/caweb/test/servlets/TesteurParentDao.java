d4e79a6efd311f6d007c0ad5ea5160414292a9cb
Yohan Gouzerh <Yohan.Gouzerh@grenoble-inp.org>
2018-04-19 16:37:39 +0200

    Test ParentDao Done
    Add test for find and delete

--- src/main/java/ensimag/caweb/test/servlets/TesteurParentDao.java
+++ src/main/java/ensimag/caweb/test/servlets/TesteurParentDao.java
@@ -21,7 +21,10 @@ import javax.servlet.http.HttpServletRequest;
 public class TesteurParentDao extends AbstractTesteur {
     
     private ParentDao parentDao;
-
+    Parent parentTest1 = new Parent(15, "toto", "test1@testing.com",
+                            "test", "One",
+                            "F", "8 street of testing",
+                            "28/02/1998", "dd/MM/yyyy", "06281548");
     @Override
     public void init() throws ServletException {
         /* Gets the unique instance of DAOFactory and then creates a new ParentDao object and stocks it in parentDao  */
@@ -31,20 +34,48 @@ public class TesteurParentDao extends AbstractTesteur {
     protected ArrayList<String> launcherTests(HttpServletRequest request) {
         ArrayList<String> results = new ArrayList<>();
         results.add(this.launchOneTest(request, "addParent", this.addParent()));
+        results.add(this.launchOneTest(request, "deleteParent", this.deleteParent()));
+        results.add(this.launchOneTest(request, "findParent", this.findParent()));
         return results;
     }
     
     /**
      * Test to insert one parent
+     * Assume that the find works
      * @return the name of the function 
      */
     private String addParent(){
-        System.out.println("add");
-        Parent parent = new Parent(15, "toto", "test1@testing.com",
-                                    "test", "One",
-                                    "F", "8 street of testing",
-                                    "28/02/1998", "dd/MM/yyyy", "06281548");
-        parentDao.add(parent);
-        return "AddParent : Ok";
+        System.out.println("add");        
+        parentDao.add(parentTest1);
+        return "AddParent : No Sql errors";
+    }
+    
+    /**
+     * Test if we found a parent
+     * Assume that the adding works
+     * @return the parent of the function
+     */
+    private String findParent(){
+        this.addParent();
+        if(parentDao.find(parentTest1.getEmail(), parentTest1.getMotDePasse()) != null){
+            return "Find : Ok";            
+        } else {
+            return "Find : No";
+        }
+    }
+    
+    /**
+     * Test to delete a parent
+     * @return the result of the test
+     */
+    private String deleteParent(){
+        System.out.println("delete");
+        addParent();
+        parentDao.delete(parentTest1.getEmail());
+        if(parentDao.find(parentTest1.getEmail(), "toto") != null){
+            return "Delete : No";
+        } else {
+            return "Delete : Ok";
+        }
     }
 }
